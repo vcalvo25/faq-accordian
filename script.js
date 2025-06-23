dfbd31b5677802338c972938f8c661595e039921
vcalvo25 <victoria.calvo1013@hotmail.com>
2025-06-22 21:12:38 -0400

    update js and add css

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const faqItems = document.querySelectorAll(".faq-item");
 
 faqItems.forEach((item) => {
-    const button = item.querySelector("button");
-    const answer = item.querySelector(".faq-answer");
+  const button = item.querySelector("button");
+  const question = item.querySelector("h3");
+  const answer = item.querySelector(".faq-answer");
+  const img = button.querySelector("img"); 
 
-    button.addEventListener("click", () => {
-        answer.classList.toggle("visible");
-    });
+  const toggleAnswer = () => {
+    const isVisible = answer.classList.toggle("visible");
+
+    if (isVisible) {
+      img.src = "./assets/images/icon-minus.svg";
+    } else {
+      img.src = "./assets/images/icon-plus.svg";
+    }
+  };
+  
+  button.addEventListener("click", toggleAnswer);
+  question.addEventListener("click", toggleAnswer);
 });
